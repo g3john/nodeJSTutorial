e0417204e94f277303c2c9b0a3c4a68cfe81456b
John Lacuna <john.lacuna@mail.utoronto.ca>
2020-03-12 21:56:36 -0400

    Added login auth

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,31 @@
 const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
-const { User, validate } = require("../models/user.model");
+const { User, validate, validateLogin } = require("../models/user.model");
 const express = require("express");
 const router = express.Router();
 
 router.get("/current", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
   res.send(user);
+});
+
+router.post("/login", async (req, res) => {
+  // validate the request body first
+  const { error } = validateLogin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //find an existing user
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid credentials.");
+  let compare = await bcrypt.compare(req.body.password, user.password);
+  if (!compare) return res.status(400).send("Invalid credentials.");
+
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+  });
 });
 
 router.post("/", async (req, res) => {
